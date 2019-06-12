94d8162ae6b12d43af5708a79293487b3ad60230
sunxiaoxuan6 <1657394323@qq.com>
2019-06-12 15:53:41 +0800

    Initial commit

---
+++ app/src/main/java/com/example/ui/DormActivity.java
@@ -0,0 +1,15 @@
+package com.example.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.ui.R;
+
+public class DormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dorm);
+    }
+}
