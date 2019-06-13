4044b386c222646a2031669cbf79fbc3fbf06f92
sunxiaoxuan6 <1657394323@qq.com>
2019-06-13 14:43:39 +0800

    界面跳转

--- app/src/main/java/com/example/ui/DormActivity.java
+++ app/src/main/java/com/example/ui/DormActivity.java
@@ -1,15 +1,66 @@
 package com.example.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 
-public class DormActivity extends AppCompatActivity {
+public class DormActivity extends AppCompatActivity  {
     private Button returns;
+    private RadioButton stay;
+    private RadioButton revise;
+    private RadioButton manage;
+    private RadioButton inquiry;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dorm);
+        init();
+    }
+
+    private void init() {
+        returns=findViewById(R.id.returns);
+        returns.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(DormActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        stay=findViewById(R.id.stay1);
+        revise=findViewById(R.id.revise1);
+        manage=findViewById(R.id.manage1);
+        inquiry=findViewById(R.id.inquiry1);
+        stay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(DormActivity.this,StayActivity.class);
+                startActivity(intent);
+            }
+        });
+        revise.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(DormActivity.this,SupervisorActivity.class);
+                startActivity(intent);
+            }
+        });
+        manage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(DormActivity.this,RoomActivity.class);
+                startActivity(intent);
+            }
+        });
+        inquiry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(DormActivity.this,SupervisorActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/ui/MainActivity.java
+++ app/src/main/java/com/example/ui/MainActivity.java
@@ -8,7 +8,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import com.example.model.Student;
+
+public class MainActivity extends AppCompatActivity {
+    int i=0;
     private Button btn_entry;
     private Button btn_revise;
     private EditText et_username;
@@ -20,50 +23,94 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        btn_entry = findViewById(R.id.btn_entry);
-        btn_revise = findViewById(R.id.btn_revise);
-        et_username = findViewById(R.id.et_username);
-        et_password = findViewById(R.id.et_password);
-        rb_student = findViewById(R.id.rb_student);
-        rb_houseparent = findViewById(R.id.rb_houseparent);
-
-        btn_entry.setOnClickListener(this);
-        btn_revise.setOnClickListener(this);
+        init();
     }
+    private void init() {
 
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.btn_entry:
-                entry();
-                break;
-            case R.id.btn_revise:
-                revise();
-                break;
+            btn_entry = findViewById(R.id.btn_entry);
+            btn_revise = findViewById(R.id.btn_revise);
+            et_username = findViewById(R.id.et_username);
+            et_password = findViewById(R.id.et_password);
+            rb_student = findViewById(R.id.rb_student);
+            rb_houseparent = findViewById(R.id.rb_houseparent);
+            //修改密码
+            btn_revise.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, RevisePassWordActivity.class);
+                    startActivity(intent);
+                }
+            });
+            //宿管界面登录
+            rb_houseparent.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                   i=0;
+                }
+            });
+            //学生界面登录
+            rb_student.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    i=1;
+                }
+            });
+            btn_entry.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(i==0){
+                        Intent intent = new Intent(MainActivity.this, DormActivity.class);
+                            startActivity(intent);
+                    }else{
+                        Intent intent = new Intent(MainActivity.this, StudentActivity.class);
+                        startActivity(intent);
+                    }
+                }
+            });
         }
     }
-    private void revise() {
+//                    btn_entry.setOnClickListener(new View.OnClickListener() {
+//                        @Override
+//                        public void onClick(View v) {
+//                            Intent intent = new Intent(MainActivity.this, StudentActivity.class);
+//                            startActivity(intent);
+//                        }
+//                    });
 
-    }
-    private void entry() {
-       int flagStudent=rb_student.isChecked()?0:1;
-       if(flagStudent==0){
-           rb_student.setOnClickListener(new View.OnClickListener() {
-               @Override
-               public void onClick(View v) {
-                   Intent intent = new Intent(MainActivity.this,StudentActivity.class);
-                   startActivity(intent);
-               }
-           });
-       } else {
-           rb_houseparent.setOnClickListener(new View.OnClickListener() {
-               @Override
-               public void onClick(View v) {
-                   Intent intent = new Intent(MainActivity.this,DormActivity.class);
-                   startActivity(intent);
-               }
-           });
-       }
-    }
-}
+
+//
+//    @Override
+//    public void onClick(View view) {
+//        switch (view.getId()) {
+//            case R.id.btn_entry:
+//                entry();
+//                break;
+//            case R.id.btn_revise:
+//                revise();
+//                break;
+//        }
+//    }
+//    private void revise() {
+//
+//    }
+//    private void entry() {
+//       int flagStudent=rb_student.isChecked()?0:1;
+//       int flagHouseparent=rb_houseparent.isChecked()?0:1;
+//       if(flagStudent==0){
+//           rb_student.setOnClickListener(new View.OnClickListener() {
+//               @Override
+//               public void onClick(View v) {
+//                   Intent intent = new Intent(MainActivity.this,StudentActivity.class);
+//                   startActivity(intent);
+//               }
+//           });
+//       } else if(flagHouseparent==0){
+//           rb_houseparent.setOnClickListener(new View.OnClickListener() {
+//               @Override
+//               public void onClick(View v) {
+//                   Intent intent = new Intent(MainActivity.this,DormActivity.class);
+//                   startActivity(intent);
+//               }
+//           });
+//       }
+//    }
